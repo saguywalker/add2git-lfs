9fb8f6d6793ef5cfa707167a314580c56ab2ea32
saguywalker <saguywalker@protonmail.com>
2019-07-24 14:33:45 +0300

    test

--- main.go
+++ main.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
+
+	"github.com/kataras/iris"
 )
+
+const uploadsDir = "./sample-files/"
 
 func uploadFile(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("File Upload Endpoint Hit")
@@ -47,11 +53,56 @@ func uploadFile(w http.ResponseWriter, r *http.Request) {
 }
 
 func setupRoutes() {
-	http.HandleFunc("/file-upload", uploadFile)
+	http.HandleFunc("/upload", uploadFile)
 	http.ListenAndServe(":8080", nil)
 }
 
 func main() {
 	fmt.Println("Hello World")
-	setupRoutes()
+	//go setupRoutes()
+	app := iris.New()
+
+	// Register templates
+	app.RegisterView(iris.HTML("./views", ".html"))
+
+	// Make the /public route path to statically serve the ./public/... contents
+	app.HandleDir("/public", "./public")
+
+	// Render the actual form
+	// GET: http://localhost:8080
+	app.Get("/", func(ctx iris.Context) {
+		ctx.View("upload.html")
+	})
+
+	// Upload the file to the server
+	// POST: http://localhost:8080/upload
+	app.Post("/upload", iris.LimitRequestBodySize(10<<20), func(ctx iris.Context) {
+		// Get the file from the dropzone request
+		file, info, err := ctx.FormFile("file")
+		if err != nil {
+			ctx.StatusCode(iris.StatusInternalServerError)
+			ctx.Application().Logger().Warnf("Error while uploading: %v", err.Error())
+			return
+		}
+
+		defer file.Close()
+		fname := info.Filename
+
+		// Create a file with the same name
+		// assuming that you have a folder named 'uploads'
+		out, err := os.OpenFile(uploadsDir+fname,
+			os.O_WRONLY|os.O_CREATE, 0666)
+
+		if err != nil {
+			ctx.StatusCode(iris.StatusInternalServerError)
+			ctx.Application().Logger().Warnf("Error while preparing the new file: %v", err.Error())
+			return
+		}
+		defer out.Close()
+
+		io.Copy(out, file)
+	})
+
+	// Start the server at http://localhost:8080
+	app.Run(iris.Addr(":8080"))
 }
